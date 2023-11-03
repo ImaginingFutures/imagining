2094040d1114854a71545d349796a2e136cddf95
jairomelo <jairoantoniomelo@gmail.com>
2023-11-03 23:16:24 +0000

    Collection detail

--- views/Details/ca_collections_default_html.php
+++ views/Details/ca_collections_default_html.php
@@ -77,6 +77,7 @@
 					<H1>{{{^ca_collections.preferred_labels.name}}}</H1>
 					<H2>{{{^ca_collections.type_id}}}{{{<ifdef code="ca_collections.idno">, ^ca_collections.idno</ifdef>}}}</H2>
 					{{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}}
+					
 <?php					
 					if ($vn_pdf_enabled) {
 						print "<div class='exportCollection'><i class='far fa-file-pdf' aria-label='"._t("Download")."'></i> ".caDetailLink($this->request, "Download as PDF", "", "ca_collections",  $vn_top_level_collection_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary'))."</div>";
@@ -94,10 +95,10 @@
 
 <?php
 				if($vb_show_objects_link){
-					print caNavLink($this->request, "<button type='button' class='btn btn-default btn-sm'><i class='fas fa-search' aria-label='Buscar'></i> Navegar dentro de la subcolección</button>", "browseRemoveFacet", "", "browse", "objects", array("facet" => "collection_facet", "id" => $t_item->get("ca_collections.collection_id")));
+					print caNavLink($this->request, "<button type='button' class='btn btn-default btn-sm'><i class='fas fa-search' aria-label='Search'></i> Search inside the Collection</button>", "browseRemoveFacet", "", "browse", "objects", array("facet" => "collection_facet", "id" => $t_item->get("ca_collections.collection_id")));
 				}
 				if($vb_show_collections_link){
-					print caNavLink($this->request, "<button type='button' class='btn btn-default btn-sm'><i class='fas fa-search' aria-label='Buscar'></i> Navegar en toda la colección</button>", "browseRemoveFacet", "", "browse", "objects", array("facet" => "collection_facet", "id" => $t_item->get("ca_collections.collection_id"))); 											
+					print caNavLink($this->request, "<button type='button' class='btn btn-default btn-sm'><i class='fas fa-search' aria-label='Search'></i> Search in all collection</button>", "browseRemoveFacet", "", "browse", "objects", array("facet" => "collection_facet", "id" => $t_item->get("ca_collections.collection_id"))); 											
 				}
 
 ?>
@@ -118,7 +119,50 @@
 ?>				
 				</div><!-- end col -->
 			</div><!-- end row -->
-			<div class="row">			
+			<div class="row">
+			<div class='col-md-6 col-lg-6'>
+			<label>People</label>
+			</div>
+		</div>
+					
+						<?php
+						$v_ents = 0;
+						
+						$qr_entities = $t_item->get('ca_entities_x_collections.entity_id', array('returnAsArray'=> true));
+						#var_dump($qr_entities);
+						$o_entities = new EntitySearch();
+						foreach($qr_entities as $qr_entity){
+							if($v_ents == 0) {print "<div class='row'>";}
+							$q_res = $o_entities->search("ca_entities.entity_id:".$qr_entity);
+							print "<div class='col-4 col-xs-4 col-sm-4 col-md-2'>";
+							while($q_res->nextHit()){
+								
+								print "<div class='entitiesTile'>";	
+								$ca_entity_media = $q_res->get('ca_object_representations.media.preview170');
+								
+								if(!$ca_entity_media){
+									$ca_entity_media = caGetThemeGraphic($this->request, "people.png");
+								}
+								print "<div class='entitiesThumbnail'>". $ca_entity_media. "</div>";
+								print caDetailLink($this->request, "<div class='entityName'>".$q_res->get('ca_entities.preferred_labels')."</div>", "", "ca_entities", $q_res->get('ca_entities.entity_id'));
+								print "</div>";
+							} print "</div>";
+							$v_ents++;
+							if ($v_ents == 4) {
+								print "</div><!-- end row -->\n";
+								$v_ents = 0;
+							}
+							
+						} if (($v_ents < 2) && ($v_ents != 0) ) {
+							print "</div><!-- end row -->\n";
+						}
+
+						
+
+						?>
+			</div>
+			<div class="row">
+
 				<div class='col-md-6 col-lg-6'>
 					{{{<ifdef code="ca_collections.description"><label>About</label>^ca_collections.description<br/></ifdef>}}}
 					{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}}
@@ -146,9 +190,6 @@
 					{{{<ifcount code="ca_collections.related" min="2"><label>Related collections</label></ifcount>}}}
 					{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.related.preferred_labels.name</l> ^relationship_typename</unit>}}}
 					
-					{{{<ifcount code="ca_entities" min="1" max="1"><label>Related person</label></ifcount>}}}
-					{{{<ifcount code="ca_entities" min="2"><label>Related people</label></ifcount>}}}
-					{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l> ^relationship_typename</unit>}}}
 					
 					{{{<ifcount code="ca_occurrences" min="1" max="1"><label>Related occurrence</label></ifcount>}}}
 					{{{<ifcount code="ca_occurrences" min="2"><label>Related occurrences</label></ifcount>}}}
@@ -159,27 +200,92 @@
 					{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l> ^relationship_typename</unit>}}}					
 				</div><!-- end col -->
 			</div><!-- end row -->
-{{{<ifcount code="ca_objects" min="2">
+			<?php
+			// Initialize the database connection and get the collection ID
+			$o_db = new Db();
+			$collection_id = $t_item->get("ca_collections.collection_id");
+
+			// Fetch object IDs associated with the collection
+			$object_id_array = [];
+			$object_ids = $o_db->query("SELECT object_id FROM ca_objects_x_collections WHERE collection_id = $collection_id");
+			while ($object_ids->nextRow()) {
+				$object_id_array[] = $object_ids->get("object_id");
+			}
+
+			// Fetch representation IDs for the objects
+			$representation_ids = [];
+			foreach ($object_id_array as $object_id) {
+				$representations = $o_db->query("SELECT representation_id FROM ca_objects_x_object_representations WHERE object_id = $object_id");
+				while ($representations->nextRow()) {
+					$representation_ids[] = $representations->get("representation_id");
+				}
+			}
+
+			// Fetch mimetypes and count them
+			$mimetype_counts = [];
+			foreach ($representation_ids as $representation_id) {
+				$mimetype_result = $o_db->query("SELECT mimetype FROM ca_object_representations WHERE representation_id = $representation_id");
+				while ($mimetype_result->nextRow()) {
+					$mimetype = $mimetype_result->get("mimetype");
+					$mimetype_counts[$mimetype] = isset($mimetype_counts[$mimetype]) ? $mimetype_counts[$mimetype] + 1 : 1;
+				}
+			}
+
+			// Define the mimetypes dictionary
+			$mimetypes = [
+				'image' => ['image/jpeg', 'image/tiff', 'image/png', 'image/x-dcraw', 'image/x-psd', 'image/x-dpx', 'image/jp2', 'image/x-adobe-dng', 'image/bmp', 'image/x-bmp'],
+				'video' => ['video/x-flv', 'video/mpeg', 'audio/x-realaudio', 'video/quicktime', 'video/x-ms-asf', 'video/x-ms-wmv', 'application/x-shockwave-flash', 'video/x-matroska', 'video/mp4'],
+				'file-pdf' => ['application/pdf'],
+				'document' => ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation'],
+				'file-audio' => ['audio/mpeg', 'audio/x-aiff', 'audio/x-wav', 'audio/wav', 'audio/mp4']
+			];
+
+			// Initialize an array to store counts for each category
+			$category_counts = [];
+
+			// Iterate through the mimetype counts and categorize them
+			foreach ($mimetype_counts as $mimetype => $count) {
+				foreach ($mimetypes as $category => $mimetype_list) {
+					if (in_array($mimetype, $mimetype_list)) {
+						$category_counts[$category] = isset($category_counts[$category]) ? $category_counts[$category] + $count : $count;
+						break;
+					}
+				}
+			}
+			?>
+
 			<div class="row">
-				<div id="browseResultsContainer">
-					<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>
-				</div><!-- end browseResultsContainer -->
-			</div><!-- end row -->
-			<script type="text/javascript">
-				jQuery(document).ready(function() {
-					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'collection_id:^ca_collections.collection_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
-						jQuery('#browseResultsContainer').jscroll({
-							autoTrigger: true,
-							loadingHtml: '<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>',
-							padding: 20,
-							nextSelector: 'a.jscroll-next'
-						});
-					});
-					
-					
+				<div class='counter'>
+					<?php
+					foreach ($category_counts as $category => $count) {
+						echo "<div class='mimetypeCat'><i class='fas fa-" . strtolower($category) . "'></i><div class='value' akhi='$count'>0</div></div>";
+					}
+					?>
+				</div>
+			</div>
+
+			<script>
+				const counters = document.querySelectorAll('.value');
+				const speed = 200;
+
+				counters.forEach(counter => {
+					const animate = () => {
+						const value = +counter.getAttribute('akhi');
+						const data = +counter.innerText;
+
+						const time = value / speed;
+						if (data < value) {
+							counter.innerText = Math.ceil(data + time);
+							setTimeout(animate, 1);
+						} else {
+							counter.innerText = value;
+						}
+					}
+
+					animate();
 				});
 			</script>
-</ifcount>}}}
+
 		</div><!-- end container -->
 	</div><!-- end col -->
 	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
