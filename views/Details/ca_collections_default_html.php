eb860e464c8a43dd625f08d595961f7d2b011cf4
jairomelo <jairoantoniomelo@gmail.com>
2023-11-06 18:29:26 +0000

    reformat

--- views/Details/ca_collections_default_html.php
+++ views/Details/ca_collections_default_html.php
@@ -153,156 +153,155 @@ $mimetypes = [
 		</div><!-- end detailNavBgLeft -->
 	</div><!-- end col -->
 	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
-		
-			<div class="row">
-				<div class='col-md-12 col-lg-12'>
-					<H1>{{{^ca_collections.preferred_labels.name}}}</H1>
-					<H2>{{{^ca_collections.type_id}}}{{{<ifdef code="ca_collections.idno">, ^ca_collections.idno</ifdef>}}}</H2>
-					{{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}}
 
-					<?php
-					if ($vn_pdf_enabled) {
-						print "<div class='exportCollection'><i class='far fa-file-pdf' aria-label='" . _t("Download") . "'></i> " . caDetailLink($this->request, "Download as PDF", "", "ca_collections",  $vn_top_level_collection_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary')) . "</div>";
-					}
-					?>
-				</div><!-- end col -->
-			</div><!-- end row -->
-
-			<div class="row">
-				<div class='col-sm-8 col-md-8 col-lg-8'>
-					<label>People</label>
-				</div>
-			</div>
-
-			<?php
-			$v_ents = 0;
-
-			$qr_entities = $t_item->get('ca_entities_x_collections.entity_id', array('returnAsArray' => true));
-			#var_dump($qr_entities);
-			$o_entities = new EntitySearch();
-			foreach ($qr_entities as $qr_entity) {
-				if ($v_ents == 0) {
-					print "<div class='row'>";
-				}
-				$q_res = $o_entities->search("ca_entities.entity_id:" . $qr_entity);
-				print "<div class='col-4 col-xs-4 col-sm-4 col-md-2'>";
-				while ($q_res->nextHit()) {
-
-					print "<div class='entitiesTile'>";
-					$ca_entity_media = $q_res->get('ca_object_representations.media.preview170');
-
-					if (!$ca_entity_media) {
-						$ca_entity_media = caGetThemeGraphic($this->request, "people.png");
-					}
-					print "<div class='entitiesThumbnail'>" . $ca_entity_media . "</div>";
-					print caDetailLink($this->request, "<div class='entityName'>" . $q_res->get('ca_entities.preferred_labels') . "</div>", "", "ca_entities", $q_res->get('ca_entities.entity_id'));
-					print "</div>";
-				}
-				print "</div>";
-				$v_ents++;
-				if ($v_ents == 4) {
-					print "</div><!-- end row -->\n";
-					$v_ents = 0;
-				}
-			}
-			if ($v_ents > 0) {
-				print "</div><!-- end row -->\n";
-			}
-
-			?>
-		<div class="col-sm-8 col-md-8 col-lg-8">
 		<div class="row">
+			<div class='col-md-12 col-lg-12'>
+				<H1>{{{^ca_collections.preferred_labels.name}}}</H1>
+				<H2>{{{^ca_collections.type_id}}}{{{<ifdef code="ca_collections.idno">, ^ca_collections.idno</ifdef>}}}</H2>
+				{{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}}
 
-			<div class='col-sm-8 col-md-8 col-lg-8'>
-				{{{<ifdef code="ca_collections.description"><label>About</label>^ca_collections.description<br/></ifdef>}}}
-				{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}}
 				<?php
-				# Comment and Share Tools
-				if ($vn_comments_enabled | $vn_share_enabled) {
-
-					print '<div id="detailTools">';
-					if ($vn_comments_enabled) {
-				?>
-						<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><i class="far fa-comment-dots" aria-label="<?php print _t("Comments and tags"); ?>"></i>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
-						<div id='detailComments'><?php print $this->getVar("itemComments"); ?></div><!-- end itemComments -->
-				<?php
-					}
-					if ($vn_share_enabled) {
-						print '<div class="detailTool"><i class="fas fa-share" aria-label="' . _t("Share") . '"></i>' . $this->getVar("shareLink") . '</div><!-- end detailTool -->';
-					}
-					print '</div><!-- end detailTools -->';
+				if ($vn_pdf_enabled) {
+					print "<div class='exportCollection'><i class='far fa-file-pdf' aria-label='" . _t("Download") . "'></i> " . caDetailLink($this->request, "Download as PDF", "", "ca_collections",  $vn_top_level_collection_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary')) . "</div>";
 				}
 				?>
-
-			</div><!-- end col -->
-			<div class='col-sm-8 col-md-8 col-lg-8'>
-				{{{<ifcount code="ca_collections.related" min="1" max="1"><label>Related collection</label></ifcount>}}}
-				{{{<ifcount code="ca_collections.related" min="2"><label>Related collections</label></ifcount>}}}
-				{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.related.preferred_labels.name</l> ^relationship_typename</unit>}}}
-
-
-				{{{<ifcount code="ca_occurrences" min="1" max="1"><label>Related occurrence</label></ifcount>}}}
-				{{{<ifcount code="ca_occurrences" min="2"><label>Related occurrences</label></ifcount>}}}
-				{{{<unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l> ^relationship_typename</unit>}}}
-
-				{{{<ifcount code="ca_places" min="1" max="1"><label>Related place</label></ifcount>}}}
-				{{{<ifcount code="ca_places" min="2"><label>Related places</label></ifcount>}}}
-				{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l> ^relationship_typename</unit>}}}
 			</div><!-- end col -->
 		</div><!-- end row -->
+
+		<div class="row">
+			<div class='col-sm-8 col-md-8 col-lg-8'>
+				<label>People</label>
+			</div>
+		</div>
+
 		<?php
-		// Initialize the database connection and get the collection ID
-		$o_db = new Db();
-		$collection_id = $t_item->get("ca_collections.collection_id");
+		$v_ents = 0;
 
-		// Fetch object IDs associated with the collection
-		$object_id_array = [];
-		$object_ids = $o_db->query("SELECT object_id FROM ca_objects_x_collections WHERE collection_id = $collection_id");
-		while ($object_ids->nextRow()) {
-			$object_id_array[] = $object_ids->get("object_id");
-		}
+		$qr_entities = $t_item->get('ca_entities_x_collections.entity_id', array('returnAsArray' => true));
+		#var_dump($qr_entities);
+		$o_entities = new EntitySearch();
+		foreach ($qr_entities as $qr_entity) {
+			if ($v_ents == 0) {
+				print "<div class='row'>";
+			}
+			$q_res = $o_entities->search("ca_entities.entity_id:" . $qr_entity);
+			print "<div class='col-4 col-xs-4 col-sm-4 col-md-2'>";
+			while ($q_res->nextHit()) {
 
-		// Fetch representation IDs for the objects
-		$representation_ids = [];
-		foreach ($object_id_array as $object_id) {
-			$representations = $o_db->query("SELECT representation_id FROM ca_objects_x_object_representations WHERE object_id = $object_id");
-			while ($representations->nextRow()) {
-				$representation_ids[] = $representations->get("representation_id");
+				print "<div class='entitiesTile'>";
+				$ca_entity_media = $q_res->get('ca_object_representations.media.preview170');
+
+				if (!$ca_entity_media) {
+					$ca_entity_media = caGetThemeGraphic($this->request, "people.png");
+				}
+				print "<div class='entitiesThumbnail'>" . $ca_entity_media . "</div>";
+				print caDetailLink($this->request, "<div class='entityName'>" . $q_res->get('ca_entities.preferred_labels') . "</div>", "", "ca_entities", $q_res->get('ca_entities.entity_id'));
+				print "</div>";
+			}
+			print "</div>";
+			$v_ents++;
+			if ($v_ents == 4) {
+				print "</div><!-- end row -->\n";
+				$v_ents = 0;
 			}
 		}
-
-		// Fetch mimetypes and count them
-		$mimetype_counts = [];
-		foreach ($representation_ids as $representation_id) {
-			$mimetype_result = $o_db->query("SELECT mimetype FROM ca_object_representations WHERE representation_id = $representation_id");
-			while ($mimetype_result->nextRow()) {
-				$mimetype = $mimetype_result->get("mimetype");
-				$mimetype_counts[$mimetype] = isset($mimetype_counts[$mimetype]) ? $mimetype_counts[$mimetype] + 1 : 1;
-			}
+		if ($v_ents > 0) {
+			print "</div><!-- end row -->\n";
 		}
 
-		
+		?>
+		<div class="col-sm-8 col-md-8 col-lg-8">
+			<div class="row">
+
+				<div class='col-sm-8 col-md-8 col-lg-8'>
+					{{{<ifdef code="ca_collections.description"><label>About</label>^ca_collections.description<br/></ifdef>}}}
+					{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}}
+					<?php
+					# Comment and Share Tools
+					if ($vn_comments_enabled | $vn_share_enabled) {
+
+						print '<div id="detailTools">';
+						if ($vn_comments_enabled) {
+					?>
+							<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><i class="far fa-comment-dots" aria-label="<?php print _t("Comments and tags"); ?>"></i>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
+							<div id='detailComments'><?php print $this->getVar("itemComments"); ?></div><!-- end itemComments -->
+					<?php
+						}
+						if ($vn_share_enabled) {
+							print '<div class="detailTool"><i class="fas fa-share" aria-label="' . _t("Share") . '"></i>' . $this->getVar("shareLink") . '</div><!-- end detailTool -->';
+						}
+						print '</div><!-- end detailTools -->';
+					}
+					?>
+
+				</div><!-- end col -->
+				<div class='col-sm-8 col-md-8 col-lg-8'>
+					{{{<ifcount code="ca_collections.related" min="1" max="1"><label>Related collection</label></ifcount>}}}
+					{{{<ifcount code="ca_collections.related" min="2"><label>Related collections</label></ifcount>}}}
+					{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.related.preferred_labels.name</l> ^relationship_typename</unit>}}}
 
 
-		// Initialize an array to store counts for each category
-		$category_counts = [];
+					{{{<ifcount code="ca_occurrences" min="1" max="1"><label>Related occurrence</label></ifcount>}}}
+					{{{<ifcount code="ca_occurrences" min="2"><label>Related occurrences</label></ifcount>}}}
+					{{{<unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l> ^relationship_typename</unit>}}}
 
-		// Iterate through the mimetype counts and categorize them
-		foreach ($mimetype_counts as $mimetype => $count) {
-			foreach ($mimetypes as $category => $mimetype_data) {
-				if (in_array($mimetype, $mimetype_data['types'])) {
-					$category_counts[$category] = isset($category_counts[$category]) ? $category_counts[$category] + $count : $count;
-					break;
+					{{{<ifcount code="ca_places" min="1" max="1"><label>Related place</label></ifcount>}}}
+					{{{<ifcount code="ca_places" min="2"><label>Related places</label></ifcount>}}}
+					{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l> ^relationship_typename</unit>}}}
+				</div><!-- end col -->
+			</div><!-- end row -->
+			<?php
+			// Initialize the database connection and get the collection ID
+			$o_db = new Db();
+			$collection_id = $t_item->get("ca_collections.collection_id");
+
+			// Fetch object IDs associated with the collection
+			$object_id_array = [];
+			$object_ids = $o_db->query("SELECT object_id FROM ca_objects_x_collections WHERE collection_id = $collection_id");
+			while ($object_ids->nextRow()) {
+				$object_id_array[] = $object_ids->get("object_id");
+			}
+
+			// Fetch representation IDs for the objects
+			$representation_ids = [];
+			foreach ($object_id_array as $object_id) {
+				$representations = $o_db->query("SELECT representation_id FROM ca_objects_x_object_representations WHERE object_id = $object_id");
+				while ($representations->nextRow()) {
+					$representation_ids[] = $representations->get("representation_id");
 				}
 			}
-		}
-		?>
 
-		
+			// Fetch mimetypes and count them
+			$mimetype_counts = [];
+			foreach ($representation_ids as $representation_id) {
+				$mimetype_result = $o_db->query("SELECT mimetype FROM ca_object_representations WHERE representation_id = $representation_id");
+				while ($mimetype_result->nextRow()) {
+					$mimetype = $mimetype_result->get("mimetype");
+					$mimetype_counts[$mimetype] = isset($mimetype_counts[$mimetype]) ? $mimetype_counts[$mimetype] + 1 : 1;
+				}
+			}
 
-	</div><!-- end container -->
-	<div class="col-sm-4 col-md-3 col-lg-3">
-	<div class="row">
+
+
+
+			// Initialize an array to store counts for each category
+			$category_counts = [];
+
+			// Iterate through the mimetype counts and categorize them
+			foreach ($mimetype_counts as $mimetype => $count) {
+				foreach ($mimetypes as $category => $mimetype_data) {
+					if (in_array($mimetype, $mimetype_data['types'])) {
+						$category_counts[$category] = isset($category_counts[$category]) ? $category_counts[$category] + $count : $count;
+						break;
+					}
+				}
+			}
+			?>
+
+
+		</div><!-- end container -->
+		<div class="col-sm-4 col-md-3 col-lg-3">
+			<div class="row">
 				<div class='col-sm-12'>
 
 					<?php
@@ -317,12 +316,15 @@ $mimetypes = [
 							if ($vb_show_collections_link) {
 								print caNavLink($this->request, "<button type='button' class='btn btn-default btn-sm'><i class='fas fa-search' aria-label='Search'></i> Search in all collection</button>", "browseRemoveFacet", "", "browse", "objects", array("facet" => "collection_facet", "id" => $t_item->get("ca_collections.collection_id")));
 							}
-
 							?>
 
 						</div>
 					<?php
+					} else {
+						print "<p>This collection has no items :(</p>";
 					}
+
+
 					if ($vb_show_hierarchy_viewer) {
 					?>
 						<div id="collectionHierarchy"><?php print caBusyIndicatorIcon($this->request) . ' ' . addslashes(_t('Loading...')); ?></div>
@@ -337,62 +339,62 @@ $mimetypes = [
 				</div><!-- end col -->
 			</div><!-- end row -->
 
-	</div>
-</div><!-- end col -->
+		</div>
+	</div><!-- end col -->
 
-<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
-	<div class="detailNavBgRight">
-		{{{nextLink}}}
-	</div><!-- end detailNavBgLeft -->
-</div><!-- end col -->
+	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
+		<div class="detailNavBgRight">
+			{{{nextLink}}}
+		</div><!-- end detailNavBgLeft -->
+	</div><!-- end col -->
 </div><!-- end row -->
 <div class="row">
-			<div class='counter col-sm-12 col-md-12 col-lg-12'>
-				<?php
-				if (!$category) {
-					echo "<div class='mimetypeCat  col-4 col-xs-4 col-sm-4 col-md-2'><i class='fas fa-folder-minus'></i><div class='value'>0</div><div class='mimeLabel'>No items yet,<br>but not for long! </div></div>";
-				}
-				$colors = ['first', 'second', 'third', 'fourth'];
+	<div class='counter col-sm-12 col-md-12 col-lg-12'>
+		<?php
+		if (!$category) {
+			echo "<div class='mimetypeCat  col-4 col-xs-4 col-sm-4 col-md-2'><i class='fas fa-folder-minus'></i><div class='value'>0</div><div class='mimeLabel'>No items yet,<br>but not for long! </div></div>";
+		}
+		$colors = ['first', 'second', 'third', 'fourth'];
 
+		$counter = 0;
+		foreach ($category_counts as $category => $count) {
+			$catData = $mimetypes[$category];
+			$colorClass = 'value ' . $colors[$counter % count($colors)];
+			if ($count > 1) {
+				$cat_label = $catData['label'] . 's';
+			} else {
+				$cat_label = $catData['label'];
+			}
+			echo "<div class='mimetypeCat'><i class='fas fa-" . strtolower($category) . "'></i><div class='$colorClass' akhi='$count'>0</div><div class='mimeLabel'>" . strtoupper($cat_label) . "</div></div>";
+
+			if ($counter == 4) {
 				$counter = 0;
-				foreach ($category_counts as $category => $count) {
-					$catData = $mimetypes[$category];
-					$colorClass = 'value ' . $colors[$counter % count($colors)];
-					if ($count > 1) {
-						$cat_label = $catData['label'] . 's';
-					} else {
-						$cat_label = $catData['label'];
-					}
-					echo "<div class='mimetypeCat'><i class='fas fa-" . strtolower($category) . "'></i><div class='$colorClass' akhi='$count'>0</div><div class='mimeLabel'>" . strtoupper($cat_label) . "</div></div>";
+			} else {
+				$counter++;
+			}
+		}
+		?>
+	</div>
+</div>
 
-					if($counter == 4){
-						$counter = 0;
-					} else {
-						$counter++;
-					}
-				}
-				?>
-			</div>
-		</div>
+<script>
+	const counters = document.querySelectorAll('.value');
+	const speed = 200;
 
-		<script>
-			const counters = document.querySelectorAll('.value');
-			const speed = 200;
+	counters.forEach(counter => {
+		const animate = () => {
+			const value = +counter.getAttribute('akhi');
+			const data = +counter.innerText;
 
-			counters.forEach(counter => {
-				const animate = () => {
-					const value = +counter.getAttribute('akhi');
-					const data = +counter.innerText;
+			const time = value / speed;
+			if (data < value) {
+				counter.innerText = Math.ceil(data + time);
+				setTimeout(animate, 1);
+			} else {
+				counter.innerText = value;
+			}
+		}
 
-					const time = value / speed;
-					if (data < value) {
-						counter.innerText = Math.ceil(data + time);
-						setTimeout(animate, 1);
-					} else {
-						counter.innerText = value;
-					}
-				}
-
-				animate();
-			});
-		</script>
+		animate();
+	});
+</script>
