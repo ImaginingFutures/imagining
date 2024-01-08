d571a79c15e07448adf2782f268f350ae45bd70c
Zabandosk <orhun.ugur93@gmail.com>
2024-01-08 14:44:16 +0000

    final changes before the new version

--- views/Details/ca_objects_default_html.php
+++ views/Details/ca_objects_default_html.php
@@ -58,7 +58,7 @@ require_once(__CA_THEMES_DIR__ . "/imagining/views/Details/data/rightsstatement.
 					{{{
 					<ifdef code="ca_objects.idno"><div class="unit">Object ID: ^ca_objects.idno
 		<button class="btn btn-default btn-xs pull-left button-circled" id="togglePanel" data-toggle="tooltip" data-placement="top" title="Media info">
-                    <span class="glyphicon glyphicon-info-sign"></span>
+		<i class="fas fa-info-circle"></i>
 					</button></div></ifdef>
 				}}}
 				</div>
@@ -95,7 +95,7 @@ require_once(__CA_THEMES_DIR__ . "/imagining/views/Details/data/rightsstatement.
 					{{{
 					<ifdef code="ca_objects.alternativetitle"><div class="unit">
 					<ifdef code="ca_objects.ai"><button class="btn btn-warning btn-xs pull-left warning-translation-button" id="togglePanel" data-toggle="tooltip" data-placement="top" title="^ca_objects.ai">
-                    <span class="glyphicon glyphicon-warning-sign"></span>
+                    <i class="fas fa-exclamation-triangle"></i>
 					</button></ifdef>
 					<label>Translated title:</label>^ca_objects.alternativetitle
 					</div><HR></ifdef>
@@ -126,7 +126,7 @@ require_once(__CA_THEMES_DIR__ . "/imagining/views/Details/data/rightsstatement.
 
 					{{{<ifdef code="ca_objects.descriptionalt"><div class="unit">
 					<ifdef code="ca_objects.ai"><button class="btn btn-warning btn-xs pull-left warning-translation-button" id="togglePanel" data-toggle="tooltip" data-placement="top" title="^ca_objects.ai">
-                    <span class="glyphicon glyphicon-warning-sign"></span>
+                    <i class="fas fa-exclamation-triangle"></i>
 					</button></ifdef>
 					<label>Translated description:</label>^ca_objects.descriptionalt					
 				</div><HR></ifdef>}}}
@@ -312,17 +312,14 @@ require_once(__CA_THEMES_DIR__ . "/imagining/views/Details/data/rightsstatement.
 					$right_id = $t_object->get("ca_objects.rights");
 
 					if ($right_id) {
-						$rights_group = $t_object->get("ca_objects.rights.related.preferred_labels");
+						echo "<label>Rights:</label>";
+						$rights_idno = $t_object->get("ca_objects.rights.idno");
 						$rights_label = $t_object->get("ca_objects.rights.preferred_labels");
 
 						$rights = new Rights();
-						$rights->rightsstatement($rights_group, $rights_label);
+						$rights->rightsstatement($rights_idno, $rights_label);
 					}
 					?>
-
-					{{{
-<ifdef code="ca_objects.rights"><div class="unit"><label>Rights:</label>^ca_objects.rights</div></ifdef>
-}}}
 
 					<!-- End of Licences amd Notices -->
 
@@ -397,7 +394,15 @@ if (match && match.length > 0) {
   console.error('No numbers found in the string');
 }
 
-var popupContent = `<a href="${detailPageURL}">ID: ${objid}<br>Title: ${titles}</a>`;
+
+var placesLabels = '^ca_places.preferred_labels';
+
+const placesLabelsArray = placesLabels.split(";");
+
+var lastPlaceLabel = placesLabelsArray[placesLabelsArray.length - 1];
+
+var popupContent = `<a href="${detailPageURL}">${lastPlaceLabel}</a>`;
+
 var marker = L.marker([lat, lon]).addTo(map);
 marker.bindPopup(popupContent);
 } catch (error) {
@@ -504,16 +509,16 @@ console.error("Error:", error);
 						if ($vn_comments_enabled) {
 					?>
 							<div class="detailTool">
-								<a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments and Tags (<?php print sizeof($va_comments) + sizeof($va_tags); ?>)</a>
+								<a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><i class="far fa-comments"></i>Comments and Tags (<?php print sizeof($va_comments) + sizeof($va_tags); ?>)</a>
 							</div><!-- end detailTool -->
 							<div id='detailComments'><?php print $this->getVar("itemComments"); ?></div><!-- end itemComments -->
 						<?php
 						}
 						if ($vn_share_enabled) {
-							print '<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>' . $this->getVar("shareLink") . '</div><!-- end detailTool -->';
+							print '<div class="detailTool"><i class="fas fa-share"></i></span>' . $this->getVar("shareLink") . '</div><!-- end detailTool -->';
 						}
 						if ($vn_pdf_enabled) {
-							print "<div class='detailTool'><span class='glyphicon glyphicon-file'></span>" . caDetailLink($this->request, "Download as PDF", "faDownload", "ca_objects",  $vn_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_objects_summary')) . "</div>";
+							print "<div class='detailTool'><i class='fas fa-file-pdf'></i></span>" . caDetailLink($this->request, "Download as PDF", "faDownload", "ca_objects",  $vn_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_objects_summary')) . "</div>";
 						}
 						?>
 						<div class='detailTool'><a href='#' onclick='caMediaPanel.showPanel("<?= caNavUrl($this->request, '', 'Lightbox', 'addItemForm', array('object_id' => $vn_id)); ?>"); return false;' title='Add to lightbox'><span class='fa fa-suitcase'></span><?= _t('Add to favorites'); ?></a></div>
@@ -523,7 +528,7 @@ console.error("Error:", error);
 
 					?>
 					<?php
-					print "<div class='inquireButton'>" . caNavLink($this->request, "<span class='glyphicon glyphicon-envelope'></span> Inquire", "btn btn-default btn-small", "", "Contact", "Form", array("table" => "ca_objects", "id" => $t_object->get("object_id"))) . "</div>";
+					print "<div class='inquireButton'>" . caNavLink($this->request, "<i class='far fa-envelope'></i> Inquire", "btn btn-default btn-small", "", "Contact", "Form", array("table" => "ca_objects", "id" => $t_object->get("object_id"))) . "</div>";
 					?>
 				</div>
 
