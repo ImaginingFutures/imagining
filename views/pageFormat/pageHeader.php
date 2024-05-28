76a2f8ad780767df1d7bddf23385f794ba38af66
jairomelo <jairomelo@gmail.com>
2024-05-28 11:25:45 +0100

    favicon

--- views/pageFormat/pageHeader.php
+++ views/pageFormat/pageHeader.php
@@ -60,6 +60,15 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php print caGetThemeAssetURL('graphics/favicon/apple-touch-icon.png') ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php print caGetThemeAssetURL('graphics/favicon/favicon-32x32.png') ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php print caGetThemeAssetURL('graphics/favicon/favicon-16x16.png') ?>">
+	<link rel="manifest" href="<?php print caGetThemeAssetURL('graphics/favicon/site.webmanifest') ?>">
+	<link rel="mask-icon" href="<?php print caGetThemeAssetURL('graphics/favicon/safari-pinned-tab.svg') ?>" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+
 	<?= AssetLoadManager::getLoadHTML($this->request); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 	
